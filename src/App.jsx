0ca57c08ab5b4c8960576430c8001e844f6e39a2
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-03-30 07:57:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './NavBar/NavBar'
+
+function App() {
+
+
+  return (
+
+   <NavBar></NavBar>
+  
+  )
+}
+
+export default App
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { CiShoppingCart } from "react-icons/ci";
+
+const NavBar = () => {
+    return (
+        <div className='max-w-300 w-full mx-auto'>
+            <div className="navbar ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content font-medium  z-1 mt-3 w-52 p-2">
+                             <li><a>Products</a></li>
+                            <li>
+                                <a>Features</a>
+                            </li>
+                            <li><a >Pricing</a></li>
+                            <li><a>Testimonials</a></li>
+                            <li><a>FAQ</a></li>
+                        </ul>
+                    </div>
+                    <a className="font-bold md:text-2xl text-xl  bg-linear-to-r from-purple-700 via-purple-600 to-purple-500 bg-clip-text text-transparent">DigiTools</a>
+                </div>
+                <div className="navbar-center  hidden lg:flex font-medium">
+                    <ul className="menu menu-horizontal px-1">
+                         <li><a>Products</a></li>
+                            <li>
+                                <a>Features</a>
+                            </li>
+                            <li><a >Pricing</a></li>
+                            <li><a>Testimonials</a></li>
+                            <li><a>FAQ</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end flex md:gap-2 gap-1 items-center">
+                    <span><CiShoppingCart  className="md:w-8 md:h-8 w-6 h-6" /></span>
+                    <p className='md:text-xl text-sm font-medium'>Login</p>
+                    <a className="btn font-medium md:px-5 md:py-5 px-2 py-0 rounded-full bg-linear-to-r from-purple-700 via-purple-600 to-purple-500 text-white ">Get Started</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
