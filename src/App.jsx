77d9d3a142f7f62eb5c1ce65197a38caaa8bc833
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-03-30 11:38:15 +0600

    SubscriptionsHeader

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Suspense } from 'react'
 import NavBar from './NavBar/NavBar'
 import Hero from './Hero/Hero'
 import Stats from './Stats/Stats'
+import SubscriptionsHeader from './Subscriptions/SubscriptionsHeader/SubscriptionsHeader'
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
     <Hero></Hero>
    
    <Stats></Stats>
+
+   <SubscriptionsHeader></SubscriptionsHeader>
 
    </>
   )
